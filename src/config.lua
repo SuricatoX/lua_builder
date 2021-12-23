c14bcba803a8e67f85b7c7816bc7138e0999ed4f
SuricatoX <luccafds@hotmail.com>
2021-12-23 02:58:58 -0300

    Config creation

---
+++ src/config.lua
@@ -0,0 +1,22 @@
+local cfg = {}
+
+cfg.preCode = [[
+local __isAuth__ = false
+
+local function sucesso(body)
+    __isAuth__ = true
+    print('['.. GetCurrentResourceName() ..'] SCRIPT AUTENTICADO COM SUCESSO')
+end
+
+local function erro(body)
+    __isAuth__ = false
+    print('['.. GetCurrentResourceName() ..'] FALHA NA AUTENTICAÇÃO')
+end
+
+local function timeout(body)
+    __isAuth__ = false
+    print('['.. GetCurrentResourceName() ..'] FALHA NA CONEXÃO COM A API')
+end
+]] -- Escreva aqui códigos estático que você quer antes da build
+
+return cfg
